0f274223dd8e4f9d12789bcc2b7eb71279b91895
Jenkins CI <ci@yourdomain.com>
2026-07-31 19:50:02 +0000

    Allure report for build #11

--- pages/base_page.py
+++
@@ -1,9 +0,0 @@
-from playwright.sync_api import Page
-
-
-class BasePage:
-    def __init__(self, page: Page):
-        self.page = page
-
-    def navigate_to(self, url: str):
-        self.page.goto(url, wait_until="domcontentloaded", timeout=120000)
--- utilities/load_json_test_data.py
+++
@@ -1,8 +0,0 @@
-import json
-import os.path
-
-def read_json(filename) -> dict:
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    filepath = os.path.join(current_dir,"..", "config", filename)
-    with open(filepath, "r") as file:
-        return json.load(file)
